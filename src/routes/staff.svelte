99496f40d83e4813e82125273d29598c8ea9a7f0
KurokuTetsuya <perumnas.good@gmail.com>
2020-10-01 17:14:37 +0700

    Remove comments

--- src/routes/staff.svelte
+++ src/routes/staff.svelte
@@ -11,7 +11,6 @@
     }
     let res = getList();
 </script>
-<div className="container">
     <div className="staff-list mt-5 mb-5 text-center">
         <h1>Staff Team</h1>
         {#await res}
@@ -47,35 +46,4 @@
                 </div>
             </section>
         {/await}
-        <!-- <section className="p-md-3 mx-md-5 text-lg-left">
-            <div id={roles.owner} className="row justify-content-center mb-5">
-                {res.owners.list.map(o => (
-                    <div className="row d-flex align-items-center border rounded py-2 mx-1 my-1 staff-item">
-                        <div className="col-4 avatar w-100 white d-flex justify-content-center align-items-center">
-                            <img src={o.displayAvatarURL} alt={o.username} className="img-fluid rounded-circle z-depth-1"></img>
-                        </div>
-                        <div className="col-8">
-                            <h6 className="font-weight-bold pt-2">{o.username}#{o.discriminator}</h6>
-                            <p>{o.role.name}</p>
-                        </div>
-                    </div>
-                ))}
-                <br/>
-            </div>
-            <div id={roles.staff} className="row justify-content-center mb-5">
-                {staff.list.map(o => (
-                    <div className="row d-flex align-items-center border rounded py-2 mx-1 my-1 staff-item">
-                        <div className="col-4 avatar w-100 white d-flex justify-content-center align-items-center">
-                            <img src={o.displayAvatarURL} alt={o.username} className="img-fluid rounded-circle z-depth-1"></img>
-                        </div>
-                        <div className="col-8">
-                            <h6 className="font-weight-bold pt-2">{o.username}#{o.discriminator}</h6>
-                            <p>{o.role.name}</p>
-                        </div>
-                    </div>
-                ))}
-                <br/>
-            </div>
-        </section> --->
     </div>
-</div>
